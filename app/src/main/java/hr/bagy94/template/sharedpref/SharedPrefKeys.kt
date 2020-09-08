ea6418d20ef5f3cb46ed3784048e55f92cad34ac
Davor Stajcer <davor.stajcer@factory.hr>
2020-09-08 16:53:31 +0200

    Initial commit

---
+++ app/src/main/java/hr/bagy94/template/sharedpref/SharedPrefKeys.kt
@@ -0,0 +1,7 @@
+package hr.bagy94.template.sharedpref
+
+internal const val DEFAULT_SHARED_PREF = ".DEFAULT_SHARED_PREF"
+internal const val KEY_APP_VERSION = "KEY_APP_VERSION"
+internal const val KEY_TOKEN = "KEY_TOKEN"
+internal const val KEY_API_URL = "KEY_API_URL"
+internal const val KEY_LANGUAGE = "KEY_LANGUAGE"
